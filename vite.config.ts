0d58ab935c9a10505f288753896096c5d9e7b1d6
Isaac Hermel <isaachermel@gmail.com>
2023-04-06 17:12:49 -0300

    PR CREATION

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+// Comentário apenas para abrir PR
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
