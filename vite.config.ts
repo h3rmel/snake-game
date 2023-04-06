6858d2587f056ca59afa942c7d991fd6e1d14945
Isaac Hermel <isaachermel@gmail.com>
2023-04-06 16:37:40 -0300

    pwa with vitejs plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
+});
